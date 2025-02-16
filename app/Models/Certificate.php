420b36ad441af8d74dcacc80e86ca225bbc02cfc
mdiktushar <md.tushar1804@gmail.com>
2025-02-16 15:02:44 +0600

    certificates

---
+++ app/Models/Certificate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Certificate extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
+
+    /**
+     *******************
+     */
+
+    /**
+     * Belongs to many users
+     * @return BelongsToMany<User, Certificate>
+     */
+    public function users():BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -160,5 +161,14 @@ class User extends Authenticatable implements JWTSubject
     public function locations(): HasMany
     {
         return $this->hasMany(Location::class);
+    }
+ 
+    /**
+     * belongs to many certificates
+     * @return BelongsToMany<Certificate, User>
+     */
+    public function certificates():BelongsToMany
+    {
+        return $this->belongsToMany(Certificate::class);
     }
 }
