57d2bf85f003d13fc7bf1ff7c9cf57262f4e46ef
mdiktushar <md.tushar1804@gmail.com>
2025-02-18 15:34:00 +0600

    route file updated

--- routes/api/v1/auth.php
+++ routes/api/v1/auth.php
@@ -7,49 +7,44 @@ use App\Http\Controllers\API\V1\Auth\PasswordController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::prefix('/v1/auth')->name('api.auth.')->group(function () {
+    // Guest routes - Accessible by unauthenticated users only
+    Route::middleware('guest:api')->group(function () {
+        // Authentication-related routes
+        Route::controller(AuthController::class)->group(function () {
+            Route::post('/login', 'login')->name('login');
+            Route::post('/register', 'register')->name('register');
+        });
 
-// Guest routes - Accessible by unauthenticated users only
-Route::middleware('guest:api')->group(function ($router) {
-    // Authentication-related routes
-    Route::controller(AuthController::class)->group(function () {
-        Route::post('/login', 'login')->name('login');
-        Route::post('/register', 'register')->name('register');
+        // Password-related routes
+        Route::controller(PasswordController::class)->group(function () {
+            Route::post('/chage-password', 'changePassword')->name('change.password');
+        });
+
+        // OTP-related routes
+        Route::prefix('/forget-password')->name('forgetpassword.')->controller(OTPController::class)->group(function () {
+            Route::post('/otp-send', 'otpSend')->name('otp.send');
+            Route::post('/otp-match', 'otpMatch')->name('otp.match');
+        });
+
+        Route::prefix('/forget-password')->name('forgetpassword.')->controller(ForgerPasswordController::class)->group(function () {
+            Route::post('/reset-password', 'resetPassword')->name('reset.password');
+        });
     });
 
-    // Password-related routes
-    Route::controller(PasswordController::class)->group(function () {
-        Route::post('/chage-password', 'changePassword')->name('change.password');
-    });
 
-    // OTP-related routes
-    Route::prefix('/forget-password')->name('forgetpassword.')->controller(OTPController::class)->group(function () {
-        Route::post('/otp-send', 'otpSend')->name('otp.send');
-        Route::post('/otp-match', 'otpMatch')->name('otp.match');
-    });
 
-    Route::prefix('/forget-password')->name('forgetpassword.')->controller(ForgerPasswordController::class)->group(function () {
-        Route::post('/reset-password', 'resetPassword')->name('reset.password');
+    // Authenticated routes - Accessible only by authenticated users
+    Route::middleware('auth:api')->group(function () {
+        // Authentication-related routes
+        Route::controller(AuthController::class)->group(function () {
+            Route::post('/logout', 'logout')->name('logout');
+            Route::post('/refresh', 'refresh')->name('refresh.token');
+        });
+        // OTP-related routes
+        Route::controller(OTPController::class)->group(function () {
+            Route::post('/otp-send', 'otpSend')->name('otp.send');
+            Route::post('/otp-match', 'otpMatch')->name('otp.match');
+        });
     });
 });
-
-
-
-// Authenticated routes - Accessible only by authenticated users
-Route::middleware('auth:api')->group(function () {
-    // Authentication-related routes
-    Route::controller(AuthController::class)->group(function () {
-        Route::post('/logout', 'logout')->name('logout');
-        Route::post('/refresh', 'refresh')->name('refresh.token');
-    });
-    // OTP-related routes
-    Route::controller(OTPController::class)->group(function () {
-        Route::post('/otp-send', 'otpSend')->name('otp.send');
-        Route::post('/otp-match', 'otpMatch')->name('otp.match');
-    });
-});
-
-
-
-
-
-
