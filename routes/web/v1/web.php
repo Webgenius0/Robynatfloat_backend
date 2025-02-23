44c1bfea72b6db28630a5a768464edcf14c2dd0e
mdiktushar <md.tushar1804@gmail.com>
2025-02-23 15:37:17 +0600

    removed un-wanted comp

--- routes/web/v1/web.php
+++ routes/web/v1/web.php
@@ -9,22 +9,12 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-
-
-
 Route::prefix('/admin')->group(function () {
     Route::get('/dashboard', function () {
         return view('backend.layouts.dashboard.index');
     })->middleware(['auth', 'web.verified'])->name('dashboard');
 
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
-
     require __DIR__ . '/auth.php';
-
     Route::name('admin.')->group(function () {
         require __DIR__ . '/users/admin.php';
         require __DIR__ . '/users/cure.php';
