5a14d9e4cc4cdf0c83c5727cf56e68a94cd8d9b0
mdiktushar <md.tushar1804@gmail.com>
2025-02-23 15:45:40 +0600

    dropdown:yatch_type controller define

--- routes/web/v1/web.php
+++ routes/web/v1/web.php
@@ -14,12 +14,16 @@ Route::prefix('/admin')->group(function () {
         return view('backend.layouts.dashboard.index');
     })->middleware(['auth', 'web.verified'])->name('dashboard');
 
-    require __DIR__ . 'auth/auth.php';
+    require __DIR__ . '/auth/auth.php';
     Route::name('admin.')->group(function () {
+        // user routes
         require __DIR__ . '/users/admin.php';
         require __DIR__ . '/users/cure.php';
         require __DIR__ . '/users/freelancer.php';
         require __DIR__ . '/users/supplier.php';
         require __DIR__ . '/users/yacth.php';
+
+        // dropdown
+        require __DIR__ . '/dropdown/yacht_type.php';
     });
 });
