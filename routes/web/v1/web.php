8d1f84a6acbd3a4c2aaeae3fe2904ddf7eff918e
mdiktushar <md.tushar1804@gmail.com>
2025-02-23 17:14:54 +0600

    foure updated

--- routes/web/v1/web.php
+++ routes/web/v1/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\Web\Backend\V1\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Web\Backend\V1\Auth\VerifyEmailController;
-use App\Http\Controllers\Web\Backend\V1\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
