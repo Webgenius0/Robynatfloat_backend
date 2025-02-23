5aae43d366b3e73a8772d97dd630ffa24f5937ea
mdiktushar <md.tushar1804@gmail.com>
2025-02-23 16:48:24 +0600

    yacht type

--- app/Http/Controllers/Web/Backend/V1/Dropdown/YachtTypeController.php
+++ app/Http/Controllers/Web/Backend/V1/Dropdown/YachtTypeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web\Backend\V1\Dropdown;
 
 use App\Http\Controllers\Controller;
+use App\Models\YachtType;
 use Illuminate\Http\Request;
 
 class YachtTypeController extends Controller
@@ -32,17 +33,9 @@ class YachtTypeController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(YachtType $yachtType)
     {
         //
     }
@@ -50,7 +43,7 @@ class YachtTypeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, YachtType $yachtType)
     {
         //
     }
@@ -58,7 +51,7 @@ class YachtTypeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(YachtType $yachtType)
     {
         //
     }
--- routes/web/v1/dropdown/yacht_type.php
+++ routes/web/v1/dropdown/yacht_type.php
@@ -1,3 +1,15 @@
 <?php
 
+use App\Http\Controllers\Web\Backend\V1\Dropdown\YachtTypeController;
 use Illuminate\Support\Facades\Route;
+
+/**
+ * route  for yacht-type
+ */
+Route::prefix('/yacht-type')->name('yacht.type')->controller(YachtTypeController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/store', 'store')->name('store');
+    Route::get('/edit/{yachtType}', 'edit')->name('edit');
+    Route::get('/update/{yachtType}', 'update')->name('update');
+    Route::get('/destroy/{yachtType}', 'destroy')->name('destroy');
+});
